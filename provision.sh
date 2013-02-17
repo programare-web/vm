0e6827569cb81f9ebed4135a68bc0c1195e4d68d
Alex Morega <alex@grep.ro>
2013-02-18 00:13:16 +0200

    set mysql root password

--- provision.sh
+++ provision.sh
@@ -3,3 +3,4 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y vim curl
 apt-get install -y libapache2-mod-php5 php5-mysql
 apt-get install -y mysql-server
+mysqladmin -u root password root
